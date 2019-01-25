a2aa74f3144fb9c459a9e057dd84b01b4f27597f
joeyhere <josephodell@outlook.com>
2019-01-25 10:54:25 +0000

    cat guitar

---
+++ lib/ascii_animation/1.rb
@@ -0,0 +1,35 @@
+'                           -:/
+         .+o+-           -/://`
+        .o+/+++`       .+///:/`                `/:
+        oo+///+//:/oyyyhs+//:/`              `+dmN
+       :y++::///oyyhdhhyyhys+:-             `dNNNh
+       -h/+:-://+osyyhsy+yssy//             /NNNo`
+       .ds//-::/+oshyo:/--/ohhs            /mmh:
+       `hdy+o+osssoshdo-``.ymso          `sNNy.
+        +yyyyyhhhhysoy+:``.-::y/        -hNmy`
+        `+yhhhds/oyy+`  `-/:.`/y.      +mNm+`
+          /ssyyoos+-`  ``.-/-:+s     `sNNm+
+           o//+oo+:.`  `````.//+`   `yNmd/
+           :/-..-::-..`` ``````-/  -hh/::/
+          `--.``              `-o-+ds.`.`-.
+         `:.`                  ./ymh.`` `-.
+         ::.`       ```  ``   `-oddo.```--
+         +-.`  ``-:/o+/-`.``  .ydms:..-/.
+        .+-.``./soo+sdy/`.```-hdds-.-/y+.
+        .:.````.:osodNh:```.+ddd+-.-+hmmm`
+        .:.`   ``..+NMNh+-/yddhy:-:smhoym`
+        .-``     `-dMNdMNmmdhydh+/ohyo/o-
+        ./.````.-/dMNhNMNdhddmhsoosyyo/`
+         /:.`` .-:sdhNNNm++mmyssssssy-
+         -so-`     `:yNmmdmNhssssossy+
+         :ymd/`      `+mmNMNosyooosshs
+        .yhhhdo.     ``smmMhsyo//--+os
+        +dNdmmmy`   ` .smNhsyyo.``.:oy
+      .:/syyyysy+.``.-+os+syys/..-:+yh:
+    .:-...+sosysyso+oo/syssoo///-/osyhy
+   .:.````../+sssss//:::/ys::/o///+oosy`
+   .--``.`-- `:+ssso:/+-//+o/: `+:/+///+
+      ```        `--://o+//:`   `::-.``:-
+                                  /``  `:-
+                                  :`   ``/
+                                   -     -'
---
+++ lib/ascii_animation/2.rb
@@ -0,0 +1,35 @@
+'            .-`              .--
+          .++++/`          :/:/.
+         `o+//+++-       -////:-               .+/
+         /++/:///ooossssyyo/:::-             .omNN
+         y/+/::///ssyddhyyhho+::            .mNNmo
+        :d+//--://osyyhsy+hssy/:            oNNd:
+        /dho////+oosyyo-/.:/oyy+           oNms.
+        :yhyssssyyy+ydh+.``:dmso         .hNmo
+        `oyyyhhdsyhyoo+:.``.::++        :dNm+
+         /yyhdds/+ss+`  `./::`-s       oNNd/
+          -o+ossos/.`  ``.-/-./+     .yNNd:
+           +::::++/-` ``````./+-    .hmdh.
+           `/..``.....`` ``.``.::  /ds:---
+           -.``                .+.odo.```-`
+          --`                  .-smd-`` `:`
+         -/.`     ``.-`` ```  `-sdds.``.:-
+         +-.```.--:oooo/`.``  -hmds--.:+`
+         +-.``./sss+omy/`.```/dddo-.-/hs/
+        -/.`````./o+dMm+.``.sddd+..:oddhN`
+        -:.`     ``/NMNmy+oyddhy+:/smy+sh`
+        -:.`   `-..yddyNNmmdhymd++oyys/+`
+        -+-.````.`````.:oysddmysooyyso-
+        `o+:--``        ``:mNsssssssy.
+         .yydmy+-.`      `-mhsysoossh:
+         -yhdhdhhho/.`  ``/Nsss+oosyh+
+        `yddmmmmyhhyyo::+yNhsyo:--:+o+
+        .hmdddhhyyyhyds+hdysyyo.``./so
+      .::+syyyyyyyysssosy+oyys/-.-:oyh.
+    .:-...-+ooysssso+o/+sys++//:-/osyho
+   --`````..::+syss//:::/ss/:/+///oooss
+   ---``.`--   ./+so//+/::/s/. .+:/+//+/
+      ```          .://o+/-.    `/--``.:.
+                                 .-``  `:.
+                                  :`   ``:
+                                  `.     -'
